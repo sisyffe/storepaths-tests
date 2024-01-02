fbbf60eaa84d2a86520fc53e4163c21aad29d33f
sisyffe <siscall.6call@gmail.com>
2024-01-02 14:53:04 +0100

    Revert "Made structure"
    This reverts commit ee04798cfe4bb4fb2146c23a169402cb553d258e.

--- tests/include/tests.h
+++
@@ -1,8 +0,0 @@
-#include "libcfgpath/common.h"
-
-LIBCFGPATH_CPP_CHECK()
-
-int testMacos(void);
-int testMacosCpp(void);
-
-LIBCFGPATH_CPP_CHECK_END()
--- tests/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "tests.h"
-
-int main() {
-    int testResult = 0x0;
-    testResult |= testMacos();
-    testResult |= testMacosCpp();
-    return testResult;
-}
